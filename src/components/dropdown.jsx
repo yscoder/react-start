cca1b447e2080ea041d9754841f82a56de0959e7
yscoder <634206017@qq.com>
2016-12-26 19:12:56 +0800

    createClass -> extends Component

--- src/components/dropdown.jsx
+++ src/components/dropdown.jsx
@@ -1,35 +1,39 @@
 import React from 'react'
-import Dropdown from 'css/dropdown'
+import Style from 'css/dropdown'
 
-export default React.createClass({
-    getInitialState() {
-        return {
+export default class Dropdown extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
             selectIndex: 0,
             show: false
         }
-    },
+    }
+
     toggle() {
         this.setState({ show: !this.state.show })
-    },
+    }
+
     onClickItem(item, i) {
         this.setState({
             selectIndex: i,
             show: !this.state.show
         })
         this.props.onChange(item, i)
-    },
+    }
+
     render() {
         return (
-            <div className={Dropdown.wrap + (this.state.show ? ' ' + Dropdown.in : '')}>
-                <button type="button" className={Dropdown.btn} onClick={this.toggle}>{this.props.data[this.state.selectIndex]}</button>
-                <ul className={Dropdown.menu}>
+            <div className={Style.wrap + (this.state.show ? ' ' + Style.in : '')}>
+                <button type="button" className={Style.btn} onClick={this.toggle.bind(this)}>{this.props.data[this.state.selectIndex]}</button>
+                <ul className={Style.menu}>
                     {
                         this.props.data.map((item, i) => (
-                            <li className={Dropdown.item} key={item} onClick={this.onClickItem.bind(this, item, i)}>{item}</li>
+                            <li className={Style.item} key={item} onClick={this.onClickItem.bind(this, item, i)}>{item}</li>
                         ))
                     }
                 </ul>
             </div>
         )
     }
-})
+}
--- src/view/app.jsx
+++ src/view/app.jsx
@@ -1,22 +1,25 @@
 import React from 'react'
 import Dropdown from 'components/dropdown'
 
-export default React.createClass({
-    getInitialState() {
-        return {
+export default class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
             selectItem: '河南省',
             data: ['河南省', '河北省', '湖南省', '湖北省']
         }
-    },
+    }
+
     onSelected(item) {
         this.setState({ selectItem: item })
-    },
+    }
+
     render() {
         return (
             <div>
-                <Dropdown data={this.state.data} onChange={this.onSelected} />
+                <Dropdown data={this.state.data} onChange={this.onSelected.bind(this)} />
                 <span> 你选择了：{this.state.selectItem}</span>
             </div>
         )
     }
-})
+}
